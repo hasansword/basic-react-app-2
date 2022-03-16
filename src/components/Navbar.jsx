d97935247aaabd3626d28487bda3338d0980692f
Hasan Kılıç <97hasankilic@gmail.com>
2022-03-16 03:50:58 +0300

    adding json data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import logo from '../images/airbnb-logo.png'
+import logo from "../images/airbnb-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
       <img src={logo} alt="Airbnb Logo" className="logo" />
       <ul>
-        <li>Basic-React-App-2</li>
+        <li>
+          <h3>Basic React App 2</h3>
+        </li>
       </ul>
     </nav>
   );
