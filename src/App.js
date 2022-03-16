8b447b44d497f38ed1758c4b7c86e9a0b637e1cd
Hasan Kılıç <97hasankilic@gmail.com>
2022-03-16 04:35:05 +0300

    using map method and props to show cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cards from './components/Cards';
-
+import Data from './data'
 function App() {
+
+  const cards = Data.map((item) => {
+    return (
+      <Cards
+          key = {item.id}
+          img = {item.coverImg}
+          rating = {item.stats.rating}
+          reviewCount = {item.stats.reviewCount}
+          location = {item.location}
+          title = {item.title}
+          price = {item.price}
+      />
+      )
+    })
+    console.log(cards);
+
+
   return (
     <div className="App">     
         <Navbar />
         <Hero/>
-        <Cards/>
+        {cards}
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+export default function Cards(props) {
+  
 
-export default function Cards() {
   return (
     <div className="card">
-      <img src={require("../images/photo-1.png")} alt="" className="card-image" />
+      <img src={require(`../images/${props.img}`)} alt="" className="card-image" />
       <div className="card-stats">
         <img src={require("../images/star.png")} alt="star" className="card-star" />
-        <span>5.0</span>
-        <span className="gray">(6) -</span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) -</span>
+        <span className="gray">{props.location}</span>
       </div>
-      <p>Life is better when you swim</p>
-      <p><span className="bold">From 199$</span> / Person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">From {props.price}$</span> / Person</p>
     </div>
   );
 }
